9100225d02485f21982d7c9e6841f5bd0e03885c
Denisexper <clasic.exper2023@gmail.com>
2025-04-02 20:26:23 -0600

    ->: endpoints terminados

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -31,7 +31,83 @@ class gamesControllers {
     }
 
     async getAllGames (req, res) {
-        
+        try {
+            
+            const games = await game.find();
+            
+            res.status(200).send({
+                message: "juesgos obtenidos con exito",
+                games
+            })
+
+        } catch (error) {
+            
+            res.status(500).send({
+                message: "Error obteniendo juegos",
+                error: error.message
+            })
+        }
+    }
+
+    async getGame (req, res) {
+        try {
+            
+            const { id } = req.params;
+
+            const game1 = await game.findById(id)
+
+            res.status(200).send({
+                message: "juego obtenido con exito",
+                game1
+            })
+        } catch (error) {
+            
+            res.status(500).send({
+                message: "Error obteniendo usuario",
+                error: error.message
+            })
+        }
+    }
+
+    async updateGame (req, res) {
+        try {
+            
+            const { id } = req.params;
+
+            const {name, image, genre, platform, releaseDate, developer, publisher, price, rating, multiplayer, createdAt} = req.body;
+
+            const newGame = await game.findByIdAndUpdate( id ,{name, image, genre, platform, releaseDate, developer, publisher, price, rating, multiplayer, createdAt}, {new: true});
+
+            res.status(200).send({
+                message: "Juego actualizado correctamente",
+                newGame
+            })
+        } catch (error) {
+            
+            res.status(500).send({
+                message: "Error actualizando el juego",
+                error: error.message
+            })
+        }
+    }
+
+    async deleteGame (req, res) {
+        try {
+            
+            const {id} = req.params;
+
+            const delGame = await game.findByIdAndDelete(id)
+
+            res.status(200).send({
+                message: "Juego eliminado correctamente"
+            })
+        } catch (error) {
+            
+            res.status(500).send({
+                message: "Error eliminando el juego",
+                error: error.message
+            })
+        }
     }
 
 }
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -6,6 +6,10 @@ const controllersGames = new gamesControllers;
 const app = express.Router();
 
 app.post("/create-game", controllersGames.createGame);
+app.get("/get-all", controllersGames.getAllGames);
+app.get("/get-one/:id", controllersGames.getGame);
+app.put("/update/:id", controllersGames.updateGame);
+app.delete("/delete/:id", controllersGames.deleteGame);
 
 export default app;
 
